3d8826bb4f19ad320070bffeb54b478a2d8e4709
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-05-27 16:56:55 -0300

    finished progam no inteface

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,11 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalServices;
+
 
 
 public class Program {
@@ -25,10 +30,21 @@ public class Program {
 		System.out.print("Return (dd/mm/yyy hh:ss: ");
 		Date finish = sdf.parse(sc.nextLine());
 		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 		
+		System.out.println("Enter price per hour: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.println("Enter price per day: ");
+		double pricePerDay= sc.nextDouble();
 		
+		RentalServices rentalService = new RentalServices(pricePerHour, pricePerDay, new BrazilTaxService());
 		
+		rentalService.processInvoice(cr);
 		
+		System.out.println("INVOICE:");
+		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
 		
 		
 		
